3b39088cab20acd7814fd92fc3bd060de4212fb5
alfarjoy <alfarjoywp@gmail.com>
2021-04-23 22:33:02 +0800

    Add project files.

---
+++ Exercise4/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise4.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory!")]
+        public string Name { get; set; }
+
+        [Required, Range(16, 99)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory!"), StringLength(200)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory!")]
+        public string Course { get; set; }
+
+        [Required, Range(1, 5)]
+        public int YearLevel { get; set; }
+    }
+}
